b5521b5e6dcc747d51f1160e58929302216fad11
Peter Koller <peter.koller@molitio.com>
2023-05-22 10:28:59 +0200

    chore(42): brand message image optimized

---
+++ components/common/style/StyledWaterSplash.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import Image from "next/image";
+
+export const StyledWaterSplash = styled(Image)`
+  position: absolute;
+  right: -80px;
+  bottom: -120px;
+  transform: rotate(-15deg);
+`;
