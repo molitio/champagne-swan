cd3543134666b75d382e1fae00f0eced1cf9e099
Peter Koller <peter.koller@molitio.com>
2023-05-29 07:00:23 +0200

    chore(42): fixed water splash positioning on services

--- components/services/style/StyledServicesCover.ts
+++ components/services/style/StyledServicesCover.ts
@@ -1,9 +1,12 @@
 import styled from "styled-components";
 import Image from "next/image";
 import { resolveThemeBreakPointValues } from "../../utils";
+import Link from "next/link";
 
 export const StyledServicesCover = styled.div`
   position: relative;
+  width: 100%;
+  height: 80vh;
 `;
 
 export const StyledServicesCoverMainTitle = styled.h1`
@@ -65,6 +68,31 @@ export const StyledServicesCoverButtonContainer = styled.div`
   display: flex;
   justify-content: center;
   margin: 2em auto;
-  width: max-content;
-  height: max-content;
+  width: min-content;
+  height: min-content;
+  outline: 3px dashed purple;
+`;
+
+export const StyledServicesCoverContactLink = styled(Link)`
+  outline: 3px dashed green;
+  position: relative;
+  top: 0;
+  padding: 0.5em 1em;
+  font-size: 1.5em;
+  font-weight: 300;
+  width: min-content;
+  height: min-content;
+  color: ${(props) => props?.theme?.palette?.text?.secondary};
+  background-color: ${(props) => props?.theme?.palette?.tertiary?.main};
+  text-shadow: ${(props) =>
+    props?.theme?.palette?.visual["aboutCoverTextShadow"]?.textShadow ?? ""};
+  box-shadow: ${(props) =>
+    props?.theme?.palette?.visual["buttonDafaultShadow"]?.boxShadow ?? ""};
+  text-decoration: none;
+  cursor: pointer;
+
+  :hover {
+    color: ${(props) => props?.theme?.palette?.tertiary?.main};
+    background-color: ${(props) => props?.theme?.palette?.text?.secondary};
+  }
 `;
