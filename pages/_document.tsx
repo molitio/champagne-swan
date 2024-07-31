dfb1e901046a7294eac9d0884d892fecb212ee41
Peter Koller <peter.koller@molitio.com>
2024-07-31 11:29:24 +0200

    chore(CS-00): fix to scripts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
 import emailjs from "@emailjs/browser";
 
 export default function Document() {
@@ -10,28 +9,30 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Fahkwang&family=Lato:ital,wght@0,100;0,400;1,300&display=swap"
           rel="stylesheet"
         />
+        <script
+          async
+          /* nonce={nonce} */
+          /* strategy="beforeInteractive" */
+          src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"
+        />
+        <script
+          /* nonce={nonce} */
+          id="emailClient"
+          /*       strategy="beforeInteractive" */
+          async
+          onLoad={() =>
+            emailjs.init(process.env.NEXT_PUBLIC_EMAILJS_API_KEY ?? "")
+          }
+        />
+        <script
+          async
+          src="https://r3.minicrm.hu/api/loader.js?60823-1y8vh5dekw08fsfrhd1i0v01i33zp8"
+        />
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
-      <Script
-        /* nonce={nonce} */
-        strategy="beforeInteractive"
-        src="https://cdn.jsdelivr.net/npm/@emailjs/browser@3/dist/email.min.js"
-      />
-      <Script
-        /* nonce={nonce} */
-        id="emailClient"
-        strategy="beforeInteractive"
-        onLoad={() =>
-          emailjs.init(process.env.NEXT_PUBLIC_EMAILJS_API_KEY ?? "")
-        }
-      />
-      <Script
-        strategy="lazyOnload"
-        src="https://r3.minicrm.hu/api/loader.js?60823-1y8vh5dekw08fsfrhd1i0v01i33zp8"
-      />
     </Html>
   );
 }
